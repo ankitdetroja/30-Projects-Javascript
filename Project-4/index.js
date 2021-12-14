4b43a5435be29a8afc744bf354871e5cf19bb5f4
ankit detroja <ankit.b.detroja@gmail.com>
2021-12-14 21:24:48 +0530

    project-4

---
+++ Project-4/index.js
@@ -0,0 +1,7 @@
+const mobnav = document.querySelector('.mobnav');
+const navbar = document.querySelector('.navbar');
+
+mobnav.addEventListener('click', function () {
+    navbar.classList.toggle('navbartoggle');
+})
+
